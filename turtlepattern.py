4858715d9a1785f74845c32c11bc9ebf8d63735f
SneaX-py <79396868+SneaX-py@users.noreply.github.com>
2021-02-21 16:13:41 +0800

    Add files via upload

---
+++ turtlepattern.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.bgcolor("red")              #change color of background
+turtle.color("blue")               #change color of pen
+for i in range(100):
+   turtle.speed(0)                 #speed of the pen
+   turtle.forward(75)
+   turtle.back(120)
+   turtle.right(20)
+   turtle.forward(75)
+   turtle.right(90)
+turtle.done()
+
